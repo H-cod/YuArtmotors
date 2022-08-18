f0512eb6842fa5cebe90088faba2bbe677980628
Hayk Nersesyan <hayknersesyanaj@gmail.com>
2022-08-18 23:57:55 +0300

    adding default settings sqlite

--- yuartmotors/settings.py
+++ yuartmotors/settings.py
@@ -79,6 +79,10 @@ WSGI_APPLICATION = 'yuartmotors.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+        'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+                   }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #     'NAME': 'yuartmotors',
