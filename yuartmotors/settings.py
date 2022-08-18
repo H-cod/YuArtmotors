5380d67020eb84435a84da4039fbc042dbccc0c9
Hayk Nersesyan <hayknersesyanaj@gmail.com>
2022-08-18 23:49:02 +0300

    django-heroku added

--- yuartmotors/settings.py
+++ yuartmotors/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,5 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+django_heroku.settings(locals())
